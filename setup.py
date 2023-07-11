459c2cc24806b2d271e640f4f85736c473ea35e9
kswain <kswain@mit.edu>
2023-07-11 14:30:58 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'pytorch-boost',
+  packages = find_packages(),
+  version = '0.1',
+  license='MIT',
+  description = 'Deep learning optimization library for PyTorch',
+  author = 'minyoungg, kswain55',
+  author_email = 'minhuh@mit.edu, kswain@mit.edu',
+  url = 'https://github.com/kswain55/pytorch-boost',
+  long_description_content_type = 'text/markdown',
+  keywords = [
+    'artificial intelligence'
+  ],
+  install_requires=[
+    'torch>=2.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Researchers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
